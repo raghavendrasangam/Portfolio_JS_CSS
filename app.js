668683d088c9ef14a240c9f0846b06b1a0c32605
raghavendrasangam <raghavendrasangam0007@gmail.com>
2024-01-23 00:28:27 +0530

    infinite scroll added

--- app.js
+++ app.js
@@ -19,8 +19,35 @@ function openmenu(){
 }
 function closemenu(){
     sidemenu.style.right = "-100%"
+}////////////////////////////////////////////////////////////////////////////////////////////////
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
 }
 
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyYtsqbY0i8u-1Dhyq6aykXQdR68GTMCNs1J-hYwesqf__CDBQ-6gAIfFI-9nMgaqej/exec'
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg")
